1946905f40265ce073736dc2ce4faa7859ba216e
EthanFrey2020 <ethan.frey.2020@student.lhschools.org>
2019-11-17 16:13:41 -0500

    Keyboard and Print lines added

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -1,10 +1,23 @@
 package com.Lickingheights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// MonthAndDayProject
+        Scanner keyboard = new Scanner(System.in);
 
-        
+        System.out.println("Enter the date you were born (Ex September 18, 2001 enter 18)");
+        int dayOfMonth = keyboard.nextInt();
+
+        System.out.println("Enter the month number you were born on (Example September is 9, March is 3 and etc.");
+        int month = keyboard.nextInt();
+
+        System.out.println("Enter the year you were born in");
+        int year = keyboard.nextInt();
+
+
+
     }
 }
